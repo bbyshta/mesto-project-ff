e88ae171688f9aac89fcd47e8148c2075e15a96f
Pavel Krat <pa67oc@gmail.com>
2024-02-23 11:32:05 +0300

    first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+//DOM узлы
 
-// @todo: Функция удаления карточки
+const cardsSection = document.querySelector(".places");
+const cardsList = cardsSection.querySelector(".places__list");
+const cardElement = cardTemplate.querySelector(".card");
 
-// @todo: Вывести карточки на страницу
+//Функция создания карточки
+
+function createCard(name, link, deleteCard) {
+  const cardCopy = cardElement.cloneNode(true);
+  const cardDeleteButton = cardCopy.querySelector(".card__delete-button");
+
+  cardCopy.querySelector(".card__image").src = link;
+  cardCopy.querySelector(".card__title").textContent = name;
+  cardDeleteButton.addEventListener("click", deleteCard);
+
+  return cardCopy;
+}
+
+// Функция удаления карточки
+
+function deleteCard(event) {
+  event.target.closest(".places__item").remove();
+}
+
+//Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  let card = createCard(item.name, item.link, deleteCard);
+  cardsList.append(card);
+});
