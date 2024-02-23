7b8d3b23085a0df44f302df69cf528b29740f040
Pavel Krat <pa67oc@gmail.com>
2024-02-24 01:52:43 +0300

    second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,20 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 //DOM узлы
 
-const cardsSection = document.querySelector(".places");
-const cardsList = cardsSection.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 const cardElement = cardTemplate.querySelector(".card");
 
 //Функция создания карточки
 
-function createCard(name, link, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardCopy = cardElement.cloneNode(true);
-  const cardDeleteButton = cardCopy.querySelector(".card__delete-button");
+  const buttonDelete = cardCopy.querySelector(".card__delete-button");
 
-  cardCopy.querySelector(".card__image").src = link;
-  cardCopy.querySelector(".card__title").textContent = name;
-  cardDeleteButton.addEventListener("click", deleteCard);
+  cardCopy.querySelector(".card__image").src = cardData.link;
+  cardCopy.querySelector(".card__image").alt = cardData.name;
+  cardCopy.querySelector(".card__title").textContent = cardData.name;
+
+  buttonDelete.addEventListener("click", deleteCard);
 
   return cardCopy;
 }
@@ -30,6 +31,6 @@ function deleteCard(event) {
 //Вывести карточки на страницу
 
 initialCards.forEach((item) => {
-  let card = createCard(item.name, item.link, deleteCard);
-  cardsList.append(card);
+  const card = createCard(item, deleteCard);
+  cardsContainer.append(card);
 });
